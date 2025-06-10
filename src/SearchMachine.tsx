b8a62531e90d5ef00d0b61cd219ae9b4ab4aa1cb
zhanghaojie <1455123087@qq.com>
2025-06-10 15:47:36 +0800

    feat: add SearchMachine component for handling search functionality with state management

---
+++ src/SearchMachine.tsx
@@ -0,0 +1,48 @@
+import { useMachine } from "@xstate/react";
+import React, { useRef } from "react";
+import { searchMachine } from "./searchMachine";
+
+export function SearchMachine() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [state, send] = useMachine(searchMachine);
+  const { loading, error, results, params } = state.context;
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          send({ type: "search", keyword: inputRef.current!.value });
+        }}
+      >
+        <input ref={inputRef} disabled={loading} />
+        <button type="submit" disabled={loading}>
+          search
+        </button>
+      </form>
+      {loading ? (
+        <div>loading...</div>
+      ) : state.matches("failure") ? (
+        <div>
+          <span style={{ color: "red" }}>{error}</span>
+          {params?.keyword.length ? (
+            <button
+              type="button"
+              onClick={() => {
+                send({ type: "retry" });
+              }}
+            >
+              retry
+            </button>
+          ) : null}
+        </div>
+      ) : (
+        <ul>
+          {results.map((r) => (
+            <li key={r}>{r}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
