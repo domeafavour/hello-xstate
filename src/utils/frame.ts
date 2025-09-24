8ba935bea6b282c3553b40a9d5a3a9defc8f4508
zhanghaojie <1455123087@qq.com>
2025-09-24 13:36:48 +0800

    feat: add isStopped flag to frame function for improved cleanup handling

--- src/utils/frame.ts
+++ src/utils/frame.ts
@@ -1,15 +1,20 @@
 export function frame(callback: (stop: () => void) => void, duration: number) {
   let raf: number;
   let previousTime: number = Date.now();
+  let isStopped = false;
 
   function cleanup() {
+    isStopped = true;
     cancelAnimationFrame(raf);
   }
 
   function _run() {
+    if (isStopped) return;
     callback(cleanup);
+    if (isStopped) return;
 
     raf = requestAnimationFrame((currentTime) => {
+      if (isStopped) return;
       // update per duration ms
       if (currentTime - previousTime > duration) {
         previousTime = currentTime;
